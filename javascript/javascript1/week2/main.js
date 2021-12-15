409b56b7f1f6792d175d15229d90508c3c9ea857
HYF <hyf@HYFs-MacBook-Pro.local>
2021-12-15 21:14:29 +0100

    getEventWeekday

--- javascript/javascript1/week2/main.js
+++ javascript/javascript1/week2/main.js
@@ -13,3 +13,28 @@ function getFullname(firstname, surname, formal, male) {
   }
   return full;
 }
+
+function getEventWeekday(todaysWeekday, days) {
+  let weeksDays = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday",
+  ];
+  for (let i = 0; i < 7; i++) {
+    if (todaysWeekday == weeksDays[i]) {
+      let p = (days % 7) + i;
+      return weeksDays[p % 7];
+    }
+  }
+}
+/*
+1) I didn't use a method to get the day of today, so I can check my
+function with more possibilities.
+2)The p variable, and p%7 were used to avoid delaing with
+numbers more than 7, because our array has only 7 elements
+which are the weeks days.
+*/
